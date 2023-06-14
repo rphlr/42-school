762d98dc0f09f7d061f0a4fbacf0466a87da9be4
rphlr <rrouille@student.42lausanne.ch>
2023-06-14 14:28:24 +0200

    Submodules updated

--- automate-git-update.sh
+++ automate-git-update.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-# Récupérer le répertoire parent du script
-base_dir=$(dirname "$0")
+# Define color codes for colored terminal output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-# Récupérer la liste des sous-modules
-submodules=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }')
+echo -e "${GREEN}Starting script to update all submodules and main repository...${NC}"
 
-# Parcourir chaque sous-module
-for submodule in $submodules; do
-    echo "Entering '$submodule'"
-    # Se déplacer dans le sous-module
-    cd "$base_dir/$submodule" || continue
-    # Ajouter les modifications
-    git add -A
-    # Vérifier s'il y a des modifications
-    if git diff --cached --quiet; then
-        echo "No changes in '$submodule'"
-    else
-        # Commit les modifications
-        git commit -m "Update submodule"
-        # Pousse les modifications vers la branche correspondante
-        git push origin HEAD
-    fi
-    # Revenir au répertoire parent
-    cd "$base_dir"
-done
+# For each submodule, recursively...
+echo -e "${GREEN}Updating submodules...${NC}"
+git submodule foreach --recursive ' 
+
+    # Print current submodule
+    echo -e "${RED}Working on submodule: $path${NC}"
+
+    # Try to switch to the main branch. If the main branch does not exist (which could happen in a detached HEAD state),
+    # create and switch to a new main branch
+    git checkout main || git checkout -b main && 
+
+    # Add all changes in the current submodule
+    git add . && 
+
+    # Commit with the message "Submodule updated"
+    git commit -m "Submodule updated" && 
+
+    # Push the changes to the main branch of the submodule
+    git push origin main || 
+
+    # If the previous commands fail (i.e., there are no changes to commit), display the message "No change to commit" and proceed to the next submodule
+    echo -e "${RED}No change to commit in submodule: $path${NC}" 
+'
+
+# Add all changes in the main repository
+echo -e "${GREEN}Updating main repository...${NC}"
+git add .
+
+# Commit with the message "Submodules updated"
+git commit -m "Submodules updated"
+
+# Push the changes to the main branch of the main repository on GitHub
+git push origin main
+
+echo -e "${GREEN}Script execution completed.${NC}"
